f365687fff1649b5f4d5a767ac4cce6b805b96e5
rajeevkr <rajeev.kumar@vuclip.com>
2018-02-02 17:51:57 +0530

    kafka practice

---
+++ src/main/java/com/rajeev/kafka/producer/SimpleProducer.java
@@ -0,0 +1,26 @@
+package com.rajeev.kafka.producer;
+
+import java.util.*;
+import org.apache.kafka.clients.producer.*;
+public class SimpleProducer {
+  
+   public static void main(String[] args) throws Exception{
+           
+      String topicName = "myfirsttopic";
+	  String key = "Key1";
+	  String value = "Value-1";
+      
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+      props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");         
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	        
+      Producer<String, String> producer = new KafkaProducer <String, String>(props);
+	
+	  ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName,key,value);
+	  producer.send(record);	       
+      producer.close();
+	  
+	  System.out.println("SimpleProducer Completed.");
+   }
+}
